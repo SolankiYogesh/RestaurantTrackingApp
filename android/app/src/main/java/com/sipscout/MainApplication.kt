6234b68fffc6f32e0f3570462939e71315c7ec2e
Yogesh Solanki <solankiyogesh3500@gmail.com>
2025-09-19 19:21:49 +0530

    chore: rename to SipScout

--- android/app/src/main/java/com/sipscout/MainApplication.kt
+++ android/app/src/main/java/com/sipscout/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.sy.restauranttracking
+package com.sipscout
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/SipScout/AppDelegate.swift
+++ ios/SipScout/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "RestaurantTracking",
+      withModuleName: "SipScout",
       in: window,
       launchOptions: launchOptions
     )
